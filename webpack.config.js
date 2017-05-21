84a07a01c2fe5759c0c72344d368f9a7cdbc79e2
sonlexqt <sonlt@dgroup.co>
2017-05-22 01:20:31 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist/assets'),
+    publicPath: '/assets',
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+      },
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './app'),
+  },
+};
